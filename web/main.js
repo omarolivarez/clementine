869626aeeaaef4119572f845da5cfecb0b386f49
omar-olivarez <olivarez.omar551@gmail.com>
2021-05-18 08:52:58 -0500

    adding some UI changes

--- web/main.js
+++ web/main.js
@@ -12,18 +12,27 @@ function setScreen(cols_list){
   var arr = cols_list;
   console.log("just got to setScreen");
   console.log(arr.length)
-  //var i;
   var select = document.getElementById("column");
   for (let step = 0; step < cols_list.length; step++) {
-    // Runs 5 times, with values of step 0 through 4.
-    console.log("in loop");
     var opt = arr[step];
     var el = document.createElement("option");
     el.textContent = opt;
     el.value = opt;
     select.add(el);
   }
-  //for (var i=0; i<cols_list.length; i++) {
-  //
-  //}​
+}
+
+function hideUnhide() {
+  console.log("just got to hideUnhide");
+  document.getElementById("input").style.display = "none";
+  document.getElementById("importHistory").style.display = "none";
+  document.getElementById("gridRadios1").style.display = "none";
+  document.getElementById("gridRadios2").style.display = "none";
+  document.getElementById("column").style.display = "none";
+  document.getElementById("statistic").style.display = "none";
+  document.getElementById("repititions").style.display = "none";
+  document.getElementById("begin").style.display = "none";
+
+  document.getElementById("progress").style.display = "none";
+
 }

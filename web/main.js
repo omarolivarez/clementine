c3b6129101152dfcafc45286f1f455ab9227c80f
omar-olivarez <olivarez.omar551@gmail.com>
2021-05-26 22:25:30 -0500

    adding icon and other UI changes

--- web/main.js
+++ web/main.js
@@ -43,11 +43,11 @@ $(document).ready(function(){
 });
 
 $(document).ready(function() {
-// SideNav Button Initialization
-$(".button-collapse").sideNav();
-// SideNav Scrollbar Initialization
-var sideNavScrollbar = document.querySelector('.custom-scrollbar');
-var ps = new PerfectScrollbar(sideNavScrollbar);
+  // SideNav Button Initialization
+  $(".button-collapse").sideNav();
+  // SideNav Scrollbar Initialization
+  var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+  var ps = new PerfectScrollbar(sideNavScrollbar);
 });
 
 function flipClicked() {
